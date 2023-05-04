70eed3377375acd31ae4b2fa20cb2f3b91032556
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-05-03 22:32:21 -0400

    controladores CRUD y control de errores

--- 2-sengunda_practica/server/app.js
+++ 2-sengunda_practica/server/app.js
@@ -2,7 +2,8 @@ import express from 'express'
 import postRoutes from './routes/posts.routes.js'
 //Creamos nuestra app
 const app = express()
-
+//middlewares 
+app.use(express.json())
 //Le pasamos las rutas 
 app.use(postRoutes)
 
--- 2-sengunda_practica/server/controllers/posts.controller.js
+++ 2-sengunda_practica/server/controllers/posts.controller.js
@@ -1,11 +1,60 @@
+import Post from "../models/Post.js"
 
+export const getPosts = async (req, res)=> {
+    try {
+        const posts = await Post.find()
+        res.send(posts)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
 
-export const getPosts = (req, res)=> res.send([])
+export const createPost = async (req, res)=> {
+    try {
+        //sacamos los datos del request
+        const {title, description} = req.body
+        //creamos un objeto con los datos 
+        const newPost = new Post({title,description})
+        console.log(newPost)
+        //cargamos nuestro objeto a la base de datos
+        await newPost.save()
+        res.send(newPost)
+    } catch (error) {
+       
+    }
+} 
+export const updatePost = async (req, res)=> {
+    try {
+        //en request prarams estara el id de la publicación que se tipeo en la ruta
+        console.log(req.params)
+        console.log(req.body)
+        //buscamos por id y lo actualizamos 
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.send(updatedPost) 
+    } catch (error) {
+        
+    }
+}
 
-export const createPost = (req, res)=> res.send('new post create')
+export const deletePost = async (req, res)=> {
+    try {
+        //buscamos por id y lo eliminamos
+        const postRemove = await Post.findByIdAndDelete(req.params.id, req.body)
 
-export const updatePost = (req, res)=> res.send('post actualizado') 
+        if (!postRemove) return res.sendStatus(404) 
+        
+        return res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
 
-export const deletePost = (req, res)=> res.send('post deleteado')
-
-export const getOne = (req, res)=> res.send('getting a new post')
+export const getOne = async (req, res)=> {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) return res.send(404)
+        return res.send(post)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
